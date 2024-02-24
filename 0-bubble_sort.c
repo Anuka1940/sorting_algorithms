2335c5cc661c17b01442685311ef52c4412e588b
Abdul Lateef Anuka <anukalateef@gmail.com>
2024-02-24 11:14:38 +0100

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort using bubble sort algorithm
+ * @array: pointer to an array of int
+ * @size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, length = size;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (j = 0; j < (size - 1); j++)
+	{
+		for (i = 0; i < (length - 1); i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		length--;
+	}
+}
